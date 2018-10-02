5aacf99388f6b834f6671eb30c9cd9768773b629
denys <voznyukden23@gmail.com>
2018-10-02 10:42:48 +0300

    upload custom report, set variables to build metadata (refactor code 6)

--- index.js
+++ index.js
@@ -3,6 +3,12 @@
 const FileTestReporter = require('./src/FileTestReporter');
 const AllureTestReporter = require('./src/AllureTestReporter');
 const config = require('./config');
+const fs = require('fs');
+
+/* json config wrapped in single quotes we need remove them before use config */
+let content = fs.readFileSync(config.googleStorageConfig.keyFilename);
+content = content.toString().replace(/'/gm, '');
+fs.writeFileSync(config.googleStorageConfig.keyFilename, content);
 
 function isUploadMode(vars) {
     return vars.some(varName => !!process.env[varName]);
