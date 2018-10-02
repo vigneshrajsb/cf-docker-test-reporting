2c370c8819ffaac2bc216dcfc7991d996a96151d
denys <voznyukden23@gmail.com>
2018-10-02 10:43:22 +0300

    upload custom report, set variables to build metadata (refactor code 7)

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     resultReportFolderName: 'allure-report',
     sourceReportFolderName: 'allure-results',
-    bucketName: process.env.BUCKET_NAME || 'pasha-codefresh',
+    bucketName: process.env.BUCKET_NAME,
     requiredGeneralVars: ['BUILD_ID', 'VOLUME_PATH'],
     requiredVarsForUploadMode: ['UPLOAD_DIR', 'UPLOAD_DIR_INDEX_FILE'],
     // directoryForUploadMaxSize in MB
