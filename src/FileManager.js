c5f1448938cd6bc15c877e4fd6084df2dbb95f88
denys <voznyukden23@gmail.com>
2018-10-17 17:46:39 +0300

    support upload one file

--- src/FileManager.js
+++ src/FileManager.js
@@ -7,26 +7,24 @@ const config = require('../config');
 const path = require('path');
 
 class FileManager {
-    static async uploadFiles({ srcDir, bucket, buildId }) {
+    static async uploadFiles({ srcDir, bucket, buildId, uploadFile, isUploadFile }) {
         return new Promise(async (res, rej) => {
             try {
-                const files = await recursiveReadSync(srcDir);
+                const files = await this._getFilesForUpload({ srcDir, uploadFile, isUploadFile });
 
                 console.log('Start upload report files');
 
                 const uploadPromises = files.map((f) => {
-                    // const pathToDeploy = `${buildId}/${path.parse(f).base}`;
-                    const pathWithoutSrcDir = f.replace(srcDir, '');
-                    const pathToDeploy = buildId + (pathWithoutSrcDir.startsWith('/') ? pathWithoutSrcDir : `/${pathWithoutSrcDir}`);
+                    const pathToDeploy = this._getFilePathForDeploy({ f, buildId, srcDir, isUploadFile, uploadFile });
 
                     return new Promise((resolve, reject) => {
                         bucket.upload(f, { destination: pathToDeploy }, (err) => {
-                            if (!err) {
-                                console.log(`File ${pathToDeploy} successful uploaded`);
-                                resolve(true);
-                            } else {
+                            if (err) {
                                 console.error(`Fail to upload file ${pathToDeploy}, error: `, err.message ? err.message : err);
                                 reject(new Error('Fail to upload file'));
+                            } else {
+                                console.log(`File ${pathToDeploy} successful uploaded`);
+                                resolve(true);
                             }
                         });
                     });
@@ -38,18 +36,14 @@ You can access it on https://g.codefresh.io/api/testReporting/${buildId}/${proce
                     res(true);
                 }, (err) => { rej(err); });
             } catch (err) {
-                if (err.errno === 34) {
-                    rej(new Error('Error while uploading files: Path does not exist'));
-                } else {
-                    rej(new Error(`Error while uploading files: ${err.message || 'Error while uploading files'}`));
-                }
+                rej(new Error(`Error while uploading files: ${err.message || 'Unknown error'}`));
             }
         });
     }
 
-    static getDirSize(pathToDir) {
+    static getDirOrFileSize(pathToResource) {
         return new Promise((res) => {
-            Exec(`du -sk ${pathToDir}`, (err, response) => {
+            Exec(`du -sk ${pathToResource}`, (err, response) => {
                 const match = response.trim().match(/^[\d\.\,]+/);
 
                 if (!match) {
@@ -71,11 +65,49 @@ Ensure that "working_directory" was specified for this step and it contains the 
             throw new Error('Error: Directory for upload is empty');
         }
 
-        if (config.directoryForUploadMaxSize < await this.getDirSize(pathToDir)) {
-            throw new Error(`Error: Directory for upload is to large, max size is ${config.directoryForUploadMaxSize} MB`);
+        if (config.uploadMaxSize < await this.getDirOrFileSize(pathToDir)) {
+            throw new Error(`Error: Directory for upload is to large, max size is ${config.uploadMaxSize} MB`);
         }
 
         return true;
+    }
+
+    static async validateUploadFile(pathToFile) {
+        if (!fs.existsSync(pathToFile)) {
+            throw new Error(`Error: FIle for upload does not exist. 
+Ensure that "working_directory" was specified for this step and it contains the file for upload`);
+        }
+
+        if (config.uploadMaxSize < await this.getDirOrFileSize(pathToFile)) {
+            throw new Error(`Error: File for upload is to large, max size is ${config.uploadMaxSize} MB`);
+        }
+
+        return true;
+    }
+
+    static validateUploadResource({ isUploadFile, uploadIndexFile, dirForUpload }){
+        if (isUploadFile) {
+            return this.validateUploadFile(uploadIndexFile);
+        } else {
+            return this.validateUploadDir(dirForUpload);
+        }
+    }
+
+    static _getFilesForUpload({ srcDir, uploadFile, isUploadFile }) {
+        if (!isUploadFile) {
+            return recursiveReadSync(srcDir);
+        } else {
+            return [uploadFile];
+        }
+    }
+
+    static _getFilePathForDeploy({ f, buildId, srcDir, isUploadFile, uploadFile }) {
+        if (!isUploadFile) {
+            const pathWithoutSrcDir = f.replace(srcDir, '');
+            return buildId + (pathWithoutSrcDir.startsWith('/') ? pathWithoutSrcDir : `/${pathWithoutSrcDir}`);
+        } else {
+            return `${buildId}/${path.parse(uploadFile).base}`;
+        }
     }
 }
 
