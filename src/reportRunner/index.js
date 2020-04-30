2565fbdda6846b8053c30de7aaf4356580548ca3
pashavictorovich <pavel@codefresh.io>
2020-04-30 14:45:23 +0300

    add minio support

--- src/reportRunner/index.js
+++ src/reportRunner/index.js
@@ -37,7 +37,6 @@ class Runner {
 
             return report;
         } catch (e) {
-            console.error(e.stack);
             console.error(e.message);
             process.exit(1);
         }
