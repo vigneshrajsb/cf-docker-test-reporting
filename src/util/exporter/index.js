0c2539f8b153e174de0a4e57165dffc3e14f3421
pashavictorovich <pavel@codefresh.io>
2020-06-22 10:24:48 +0300

    change export way

--- src/util/exporter/index.js
+++ src/util/exporter/index.js
@@ -3,7 +3,7 @@ const exec = require('util').promisify(require('child_process').exec);
 class Exporter {
 
     async export({ key,  value }) {
-        const { stderr } = await exec(`/codefresh/volume/cf_export ${key}="${value}"`);
+        const { stderr } = await exec(`echo ${key}=${value} >> /meta/env_vars_to_export`);
         if (stderr) {
             throw new Error(`Error occurred while executing cf-export: ${stderr}`);
         } else {
